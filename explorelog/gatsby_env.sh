cbad248fa8521ee84c021f9986af71e469bc890e
Vincent Agriesti <crazychenz@gmail.com>
2020-11-27 14:43:43 +0000

    Added docker build for running gatsby,,

---
+++ explorelog/gatsby_env.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+docker run \
+    --network host \
+    -e REACT_NATIVE_PACKAGER_HOSTNAME=10.0.0.85 \
+    --rm -ti \
+    -v $(pwd):/workspace \
+    -v $(pwd)/_env/home/user:/home/user \
+    crazychenz/gatsby \
+    bash -li -c "${*}"
+
+
+    #--name sayok-test
+    #--hostname sayok-test
+    # -p 19000:19000
+    # -p 19002:19002
+    # -p 19006:19006
