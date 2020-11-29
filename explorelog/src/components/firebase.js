f9f0af77be992927670bb498ab011dac06258aa6
Vincent Agriesti <crazychenz@gmail.com>
2020-11-29 19:18:20 +0000

    Commented out firebase includes.

--- explorelog/src/components/firebase.js
+++ explorelog/src/components/firebase.js
@@ -1,7 +1,7 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/functions";
-import "firebase/storage";
+// import firebase from "firebase/app";
+// import "firebase/auth";
+// import "firebase/functions";
+// import "firebase/storage";
 
 // var firebaseConfig = {
 //   apiKey: "AIzaSyBK496vRixZjkozxhSZPMpePfPvTfS6Ftc",
